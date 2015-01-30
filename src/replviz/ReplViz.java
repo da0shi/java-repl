4249479ec4a8dd9996792b2ebbc35d383d3c03da
da0shi <whit3.develop@gmail.com>
2015-01-30 11:30:14 +0900

    update frame size

--- src/replviz/ReplViz.java
+++ src/replviz/ReplViz.java
@@ -111,7 +111,7 @@ public class ReplViz
 		JFrame frame = new JFrame();
 		frame.getContentPane().add(this);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(640, 800);
+		frame.setSize(540, 520);
 		frame.setTitle(TITLE);
 		// frame.pack();
 
